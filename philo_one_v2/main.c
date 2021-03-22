5407e595aae57e80fd94a7d2760135faad90056e
thallard <thallard@student.42lyon.fr>
2021-03-22 13:56:21 +0100

    Updated Makefile + norminette utils + refactoring main.c

--- philo_one_v2/main.c
+++ philo_one_v2/main.c
@@ -6,72 +6,13 @@
 /*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 13:39:20 by thallard          #+#    #+#             */
-/*   Updated: 2021/03/22 12:47:38 by thallard         ###   ########lyon.fr   */
+/*   Updated: 2021/03/22 13:49:26 by thallard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo_one.h"
+#include "includes/philo_one.h"
 
-pthread_mutex_t ft_create_fork(t_global *g)
-{
-	pthread_mutex_t mutex;
 
-	if (pthread_mutex_init(&mutex, NULL))
-		ft_exit(g);
-	return (mutex);
-}
-
-int ft_fill_forks(t_global *g)
-{
-	int i;
-
-	i = -1;
-	if (!(g->forks = malloc_lst(sizeof(pthread_mutex_t) * (g->info->nb_philo + 1), g)))
-		ft_exit(g);
-	while (++i < g->info->nb_philo)
-		g->forks[i] = ft_create_fork(g);
-	// dprintf(1, "res de i et total = %d %ld\n", i, g->info->nb_philo);
-	return (0);
-}
-
-pthread_mutex_t *ft_fill_mutex(t_global *g)
-{
-	int i;
-	pthread_mutex_t *forks;
-	i = -1;
-	if (!(forks = malloc_lst(sizeof(pthread_mutex_t) * (g->info->nb_philo + 1), g)))
-		ft_exit(g);
-	while (++i < g->info->nb_philo)
-		forks[i] = ft_create_fork(g);
-	// dprintf(1, "res de i et total = %d %ld\n", i, g->info->nb_philo);
-	return (forks);
-}
-
-pthread_t ft_create_thread(t_global *global)
-{
-	pthread_t thread;
-
-	if (!(thread = malloc_lst(sizeof(pthread_t), global)))
-		ft_exit(global);
-	return (thread);
-}
-
-int ft_fill_threads(t_global *global)
-{
-	int i;
-
-	i = -1;
-	if (!(global->threads = malloc_lst(sizeof(pthread_t) * (global->info->nb_philo + 2000), global)))
-		ft_exit(global);
-	while (++i < global->info->nb_philo)
-	{
-		// dprintf(1, "dbeug de i = %d\n", i);
-		global->threads[i] = ft_create_thread(global);
-	}
-
-	global->threads[i] = NULL;
-	return (1);
-}
 
 int wait_threads(t_global *global)
 {
@@ -147,7 +88,7 @@ int check_eats(t_global *g)
 		if (g->philos[j]->times_eat != g->info->nb_eat)
 			return (0);
 	}
-	dprintf(1, "fin normale\n\n\n");
+	dprintf(1, "fin normale\n");
 
 	return (1);
 }
@@ -190,7 +131,6 @@ void ft_sleep(t_philos *p)
 	pthread_mutex_lock(&p->locks[p->pos - 1]);
 	usleep(p->info->time_sleep * 1000);
 	pthread_mutex_unlock(&p->locks[p->pos - 1]);
-//	p->tdie += p->info->time_sleep;
 }
 
 void ft_think(t_philos *p)
@@ -198,39 +138,26 @@ void ft_think(t_philos *p)
 	pthread_mutex_lock(&p->safe[p->pos - 1]);
 	printf("\e[33m%.f \e[96m%ld \e[0;35mis thinking\e[39m\n", ft_time_p(p, 1), p->pos);
 	pthread_mutex_unlock(&p->safe[p->pos - 1]);
-	//usleep(p->info->time_sleep * 1000);
-	
 }
 
 void *function_thread(void *ptr)
 {
-
-	t_philos *p;
+	t_philos	*p;
 
 	p = ptr;
-	int i;
-
-	i = -1;
-	//dprintf(1, "%ld\n", p->pos - 1);
-
-	while (p->times_eat < p->info->nb_eat)
+	while (p->times_eat < p->info->nb_eat && p->times_eat != -1)
 	{
-		// usleep(10);
 		if (p->tdie <= ft_time_p(p, 1))
 		{
 			p->alive = 0;
-			//printf("\e[33m%.f \e[96m%ld \033[0;31mdied\e[39m\n", ft_time_p(p, 1), p->pos);
 			return (NULL);
 		}
-		// dprintf(1, "debug miam miam %ld %ld\n", p->times_eat, p->info->nb_eat);
 		ft_take_forks(p);
 		if (p->times_eat >= p->info->nb_eat)
 			return (NULL);
 		ft_sleep(p);
 		ft_think(p);
-	}
-	// dprintf(1, "fin du programme %ld et la bouffe %ld\n", p->pos, p->times_eat);
-	return (NULL);
+	}	return (NULL);
 }
 
 int main(int argc, char **argv)
@@ -256,31 +183,23 @@ int main(int argc, char **argv)
 	global->lock = ft_fill_mutex(global);
 	global->safe = ft_fill_mutex(global);
 	global->philos = malloc_lst(sizeof(t_philos *) * (info->nb_philo + 1), global);
-	int i = -1;
-	// while (++i < info->nb_philo)
-	// {
-	// 	global->philos[i] = ft_init_philos(global, i);
-	// }
+	int		i;
+
 	i = -1;
 	while (++i < info->nb_philo)
 	{
 		global->philos[i] = ft_init_philos(global, i);
-		global->i = i;
 		pthread_create(&global->philos[i]->thread, NULL, function_thread, (void *)global->philos[i]);
-		// usleep(20);
 		pthread_detach(global->philos[i]->thread);
 		pthread_join(global->philos[i]->thread, NULL);
 	}
 	i = -1;
 	int loop = 1;
-
 	while (loop)
 	{
 		i = -1;
 		while (++i < info->nb_philo)
 		{
-
-			// dprintf(1, "debug = %d\n", check_eats(global));
 			if (check_eats(global))
 			{
 				dprintf(1, "ils ont fini d emanger, fin du pgraomme\n");
---
+++ philo_one_v2/utils/malloc_free_and_exit.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc_free_and_exit.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/16 13:53:01 by thallard          #+#    #+#             */
+/*   Updated: 2021/03/22 12:58:19 by thallard         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo_one.h"
+
+void	*malloc_lst(int size, t_global *global)
+{
+	void		*ptr;
+
+	if (!(ptr = malloc(size)))
+		return (NULL);
+	if (!(add_lst_to_free(global, ptr)))
+		return (NULL);
+	return (ptr);
+}
+
+void	*add_lst_to_free(t_global *global, void *ptr)
+{
+	t_malloc	*new;
+
+	if (!(new = ft_lstmalloc_new(ptr)))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ft_lstmalloc_add_back(&global->lst_free, new);
+	return (ptr);
+}
+
+int		ft_exit(t_global *g)
+{
+	ft_lstmalloc_clear(&g->lst_free, free);
+		printf("Error\n");
+	exit(0);
+}
